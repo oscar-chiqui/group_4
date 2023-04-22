b6ad8fbb9da2f0bd8277dcd6947ce257fe75583b
= <nkpratt04@gmail.com>
2023-04-22 12:05:18 -0500

    Draft view to handle account information updating

--- lmn/views/views_users.py
+++ lmn/views/views_users.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 
-from ..forms import UserRegistrationForm
+from ..forms import UserRegistrationForm, UserUpdateForm
 from ..models import Note
 
 
@@ -23,6 +23,27 @@ def my_user_profile(request):
     # TODO - editable version for logged-in user to edit their own profile
     return redirect('user_profile', user_pk=request.user.pk)
 
+
+@login_required
+def edit_user_account_info(request):
+
+    if request.method == "POST":
+        user = User.objects.get(pk=request.user.pk)
+        form = UserUpdateForm(request.POST, instance=user)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account information has been successfully updated!')
+            return redirect('user_profile', user_pk=request.user.pk)
+        else:
+            messages.add_message(request, messages.INFO, 'Please check the data you entered')
+            # include the invalid form, which will have error messages added to it. 
+            # The error messages will be displayed by the template.
+            return render(request, 'lmn/users/edit_user_account_info.html', {'form': form})
+
+    form = UserUpdateForm()
+    return render(request, 'lmn/users/edit_user_account_info.html', {'form': form})
+    
 
 def register(request):
     """ Handles user registration flow

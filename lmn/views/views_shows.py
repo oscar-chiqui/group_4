2f0f8fa148fa9d4251c4deefe8a9a9ff0e4e868f
= <nkpratt04@gmail.com>
2023-04-26 23:17:06 -0500

    Exclude shows from shows with most notes list if they have 0 notes

--- lmn/views/views_shows.py
+++ lmn/views/views_shows.py
@@ -8,8 +8,8 @@ def shows_with_most_notes(request):
     """ Get the the Shows with the top ten count of notes.
     Also, get all of the notes associated with each Show. """
 
-    # Get the top ten shows with the most notes by getting the count and ordering by number of notes. 
-    top_10_shows = Show.objects.annotate(num_notes=Count('note')).order_by('-num_notes')[:10]  # Adapted from Django docs https://docs.djangoproject.com/en/4.2/topics/db/aggregation/
+    # Get the top ten shows with the most notes by getting the count and ordering by number of notes. Exclude shows with 0 notes.
+    top_10_shows = Show.objects.annotate(num_notes=Count('note')).exclude(num_notes=0).order_by('-num_notes')[:10]  # Adapted from Django docs https://docs.djangoproject.com/en/4.2/topics/db/aggregation/
     
     for show in top_10_shows:
         show.notes = show.note_set.all() # Get data on the Note objects associated with each Show

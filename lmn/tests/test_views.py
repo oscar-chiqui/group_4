928734d30e12b185b464ba461c9d04ea9ad26a50
= <nkpratt04@gmail.com>
2023-04-26 23:32:06 -0500

    Add test to ensure that shows with the most notes template is used

--- lmn/tests/test_views.py
+++ lmn/tests/test_views.py
@@ -462,6 +462,16 @@ class TestNotes(TestCase):
         self.assertTemplateUsed(response, 'lmn/notes/new_note.html')
 
 
+class TestShows(TestCase):
+    # Have to add Notes and Users and Show, and also artists and venues because of foreign key constrains in Show
+    fixtures = ['testing_users', 'testing_artists', 'testing_venues', 'testing_shows', 'testing_notes']
+
+    def test_list_shows_most_notes_correct_template_used(self): 
+
+        response = self.client.get(reverse('shows_with_most_notes'))
+        self.assertTemplateUsed(response, 'lmn/shows/shows_with_most_notes.html')
+
+
 class TestUserAuthentication(TestCase):
     """ Some aspects of registration (e.g. missing data, duplicate username) covered in test_forms """
     """ Currently using much of Django's built-in login and registration system """

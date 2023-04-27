2afcdbe850a2d9503d6be2f5d6e118e5f6ddf255
= <nkpratt04@gmail.com>
2023-04-26 21:01:30 -0500

    Make first/last name and email required in the edit account form

--- lmn/forms.py
+++ lmn/forms.py
@@ -79,3 +79,10 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Taken from Django docs
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+        self.fields['email'].required = True

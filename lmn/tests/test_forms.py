1f46c66706720c001d236b3980c0fbd31e05f236
= <nkpratt04@gmail.com>
2023-04-27 13:51:37 -0500

    Test to ensure that UserUpdateForm is invalid if user tries to edit username to one already exists in database (case-insensitive)

--- lmn/tests/test_forms.py
+++ lmn/tests/test_forms.py
@@ -263,3 +263,22 @@ class UserUpdateFormTests(TestCase):
 
         form = UserUpdateForm(form_data)
         self.assertFalse(form.is_valid())
+
+    def test_edit_user_with_username_already_in_db_case_insensitive_fails(self):
+        # Create a user with username bob
+        bob = User(username='bob', email='bob@bob.com')
+        bob.save()
+
+        invalid_username = ['BOB', 'BOb', 'Bob', 'bOB', 'bOb', 'boB']
+
+        for invalid in invalid_username:
+            # Attempt to edit another user to have same username
+            form_data = {
+                'username': invalid, 
+                'email': 'another_bob@bob.com', 
+                'first_name': 'bob', 
+                'last_name': 'whatever', 
+            }
+
+            form = UserUpdateForm(form_data)
+            self.assertFalse(form.is_valid())

ac37e527a5ae74875c9861f94fa5597513d074ef
= <nkpratt04@gmail.com>
2023-04-28 12:50:49 -0500

    Change shows_with_most_notes view to hold a max of 5 top shows instead of 10

--- lmn/views/views_shows.py
+++ lmn/views/views_shows.py
@@ -5,13 +5,13 @@ from ..models import Show
 
 
 def shows_with_most_notes(request):
-    """ Get the the Shows with the top ten count of notes.
+    """ Get the the Shows with the top 5 count of notes.
     Also, get all of the notes associated with each Show. """
 
-    # Get the top ten shows with the most notes by getting the count and ordering by number of notes. Exclude shows with 0 notes.
-    top_10_shows = Show.objects.annotate(num_notes=Count('note')).exclude(num_notes=0).order_by('-num_notes')[:10]  # Adapted from Django docs https://docs.djangoproject.com/en/4.2/topics/db/aggregation/
+    # Get the top 5 shows with the most notes by getting the count and ordering by number of notes. Exclude shows with 0 notes.
+    top_5_shows = Show.objects.annotate(num_notes=Count('note')).exclude(num_notes=0).order_by('-num_notes')[:5]  # Adapted from Django docs https://docs.djangoproject.com/en/4.2/topics/db/aggregation/
     
-    for show in top_10_shows:
+    for show in top_5_shows:
         show.notes = show.note_set.all() # Get data on the Note objects associated with each Show
 
-    return render(request, 'lmn/shows/shows_with_most_notes.html', {'top_10_shows': top_10_shows})
+    return render(request, 'lmn/shows/shows_with_most_notes.html', {'top_5_shows': top_5_shows})

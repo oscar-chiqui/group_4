a480ffd191d9a522c012eb4823116d99680a7bc5
= <nkpratt04@gmail.com>
2023-05-02 13:38:06 -0500

    Switch order of Show objects to first order by most recent date and then by number of notes

--- lmn/views/views_shows.py
+++ lmn/views/views_shows.py
@@ -5,11 +5,12 @@ from ..models import Show
 
 
 def shows_with_most_notes(request):
-    """ Get the the Shows with the top 5 count of notes.
+    """ Get the the most recent Shows with the top 5 count of notes.
     Also, get all of the notes associated with each Show. """
 
-    # Get the top 5 shows with the most notes by getting the count and ordering by number of notes. Exclude shows with 0 notes.
-    top_5_shows = Show.objects.annotate(num_notes=Count('note')).exclude(num_notes=0).order_by('-num_notes')[:5]  # Adapted from Django docs https://docs.djangoproject.com/en/4.2/topics/db/aggregation/
+    # Get the top 5 shows with the most notes by getting the count and ordering first by most recent show date, then number of notes. 
+    # Exclude shows with 0 notes.
+    top_5_shows = Show.objects.annotate(num_notes=Count('note')).exclude(num_notes=0).order_by('-show_date', '-num_notes')[:5]  # Adapted from Django docs https://docs.djangoproject.com/en/4.2/topics/db/aggregation/
     
     for show in top_5_shows:
         show.notes = show.note_set.all() # Get data on the Note objects associated with each Show
